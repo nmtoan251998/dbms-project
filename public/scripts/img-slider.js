c6bfb787cc52df977e357daa30e6e1ac2c77782e
B1605369 <toanb1605369@student.ctu.edu.vn>
2018-11-05 16:59:47 +0700

    Complete the index-page UI

---
+++ public/scripts/img-slider.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let imgSlider = document.querySelector('.img-slider');
+    let images = document.querySelectorAll('.img');
+
+    let arrowLeft = document.querySelector('.left');
+    let arrowRight = document.querySelector('.right');
+
+
+    //hidden all images
+    for(let x of images){
+        x.style.display = "none";
+    }
+
+    console.log({arrowLeft, arrowRight})
+
+    arrowLeft.addEventListener('click', previousImg);
+    arrowRight.addEventListener('click', nextImg);
+
+    
+    //set the index for recent image
+    let recentIndex = 0;
+
+    //display the first image
+    images[0].style.display = "block";
+
+    function nextImg(){
+
+        if(recentIndex === images.length-1){                        
+            //if the recent index  === length, hidden the earlier image
+            images[recentIndex].style.display = "none";
+
+            //set it to the first index
+            recentIndex = 0;
+
+            //display the first index
+            images[recentIndex].style.display = "block";
+            return
+        }
+        else{            
+            //hide the recent image
+            images[recentIndex].style.display = "none";
+
+            //increase the index by one
+            recentIndex++;
+
+            //display the image at increased index
+            images[recentIndex].style.display = "block";
+            return
+        }
+    }    
+
+    function previousImg(){                              
+        if(recentIndex === 0){            
+            //if the recent index  === length, hidden the earlier image            
+            images[recentIndex].style.display = "none";
+
+            //set it to the first index
+            recentIndex = 0;
+
+            //display the first index
+            images[recentIndex].style.display = "block";
+            return
+        }
+        else{            
+            //hide the recent image
+            images[recentIndex].style.display = "none";
+
+            //increase the index by one
+            recentIndex++;
+
+            //display the image at increased index
+            images[recentIndex].style.display = "block";
+            return
+        }
+    }
+    
+
+}, false)
