971c417038913642a2370d4c0699409588ee92e0
B1605369 <toanb1605369@student.ctu.edu.vn>
2018-11-16 16:21:42 +0700

    Restucture the UI - Clear comment in each files (controller's'.js-route's'.js-router.js) to use new Features (Class-Course-ScoreRecord)

---
+++ routes/class-route.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const majorRouter = require('../controllers/class-controller');
+
+// //major routing
+// router.get('/class/data', classRouter.getClassData);
+// router.get('/class/create', classRouter.getClassCreate);
+
+// module.exports = router;
---
+++ routes/course-route.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const majorRouter = require('../controllers/course-controller');
+
+// //major routing
+// router.get('/course/data', courseRouter.getCourseData);
+// router.get('/course/create', courseRouter.getCourseCreate);
+
+// module.exports = router;
