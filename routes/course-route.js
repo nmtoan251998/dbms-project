02001a61e4b619d7a5a4ebe8559d3dba6884fb24
B1605369 <toanb1605369@student.ctu.edu.vn>
2018-11-26 00:34:31 +0700

    Change routes

--- routes/course-route.js
+++ routes/course-route.js
@@ -3,15 +3,12 @@ const router = express.Router();
 
 const courseRouter = require('../controllers/course-controller');
 
-//major routing
-
-router.get('/course/data', courseRouter.getCourseData);
-router.post('/course/create',courseRouter.postCourseCreate);
-router.get('/course/data-create', courseRouter.getCourseCreate);
-router.get('/course/data-delete/:id', courseRouter.deleteCourseData);
-router.get('/course/data-modify/:courseid&:coursename&:startyear&:endyear', courseRouter.modifyCourseData);
-router.post('/course/data-modify/:id', courseRouter.postModifyCourseData);
-router.get('/course/data-search',courseRouter.getSearchCourseData);
-
+router.get('/data', courseRouter.getCourseData);
+router.post('/create',courseRouter.postCourseCreate);
+router.get('/data-create', courseRouter.getCourseCreate);
+router.get('/data-delete/:id', courseRouter.deleteCourseData);
+router.get('/data-modify/:courseid&:coursename&:startyear&:endyear', courseRouter.modifyCourseData);
+router.post('/data-modify/:id', courseRouter.postModifyCourseData);
+router.get('/data-search',courseRouter.getSearchCourseData);
 
 module.exports = router;
