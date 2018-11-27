211980359caed28b6bd3bd350f21067d60cf4eac
chuunibyouruna <storedatangoc@gmail.com>
2018-11-27 18:50:10 +0700

    add score & studentdetail page

--- controllers/scoreRecord-controller.js
+++ controllers/scoreRecord-controller.js
@@ -1,4 +1,5 @@
 const conn = require('../public/scripts/config');
+const find = require('../admin_modules/find');
 
 module.exports.getScoreRecordData =  (req,res) => {                
     // PAGINATION    
@@ -17,15 +18,47 @@ module.exports.getScoreRecordData =  (req,res) => {
     
     let begin = (currentPage-1) * itemPerPage;
     let end = currentPage* itemPerPage ;
+    var sql,
+        sort='desc';
+    switch (req.query.column) {
+        case 'studentid':
+            makeSort('studentid',req.query.sort);
+            break;
+        case 'subjectid':
+            makeSort('subjectid',req.query.sort);
+            break;
+        case 'year':
+            makeSort('year',req.query.sort);
+            break;
+        case 'semester':
+            makeSort('semester',req.query.sort);
+            break;
+        case 'score':
+            makeSort('score',req.query.sort);
+            break;
+        default:
+            makeSort('studentid',req.query.sort);
+            break;
+    }
     
-    const sql =`SELECT * FROM SCORE_RECORD`;
+    function makeSort(column,state){
+        if(state === 'desc'){
+            sql =`SELECT * FROM SCORE_RECORD ORDER BY ${column} DESC`;
+            sort='asc'
+        }
+        else{
+            sql =`SELECT * FROM SCORE_RECORD ORDER BY ${column} ASC`;
+        }
+    }
     
     conn.query(sql, (err,result) => {
         if(err) throw err;                    
         res.render('../views/scoreRecord/scoreRecord-data-view',{                
-            listFalculty: result.slice(begin, end),
+            listScoreRecord: result.slice(begin, end),
             page : currentPage,
-            item : itemPerPage                
+            item : itemPerPage,
+            total: result.length,
+            sort: sort                
         });
     });    
 
@@ -35,6 +68,63 @@ module.exports.getScoreRecordCreate = (req,res) => {
     res.render('../views/scoreRecord/scoreRecord-create-view');
 }
 
-module.exports.getScoreRecordModify = (req,res) => {
-    res.render('../views/scoreRecord/scoreRecord-modify-view');
+module.exports.postScoreRecordCreate = (req,res) =>{
+    console.log(req.body)
+    if (req.body.studentid && req.body.subjectid) {
+        const sql =`INSERT INTO SCORE_RECORD VALUES ('${req.body.subjectid}','${req.body.studentid}','${req.body.year}','${req.body.semester}','${req.body.score}')`;
+        conn.query(sql,(err,result)=>{
+            if(err) throw err;
+            console.log('call me',result);
+        });
+    }
+    res.redirect('/scoreRecord/data');
+}
+
+module.exports.deleteScoreRecordData = (req,res) =>{
+    console.log('ggwp',req.params);
+    var tempDelete = req.params.id.split('&');
+    const sql = `DELETE FROM SCORE_RECORD WHERE SUBJECTID = '${tempDelete[0]}' AND STUDENTID = '${tempDelete[1]}'`;
+    conn.query(sql,(err,result)=>{
+        if (err) throw err;
+        console.log(result);   
+    });
+
+    res.redirect('/scoreRecord/data');
+     
+};
+
+module.exports.modifyScoreRecordData = (req,res) =>{
+    res.render('./scoreRecord/scoreRecord-modify-view',{
+        data: req.params
+    });
+};
+
+module.exports.postModifyScoreRecordData = (req,res) =>{
+    console.log(req.body)
+    const sql = `UPDATE SCORE_RECORD SET SUBJECTID = '${req.body.subjectid}', STUDENTID = '${req.body.studentid}', YEAR = '${req.body.year}' , SEMESTER = '${req.body.semester}', SCORE = '${req.body.score}' WHERE SUBJECTID = '${req.body.subjectid}' AND STUDENTID = '${req.body.studentid}'`;
+   
+    conn.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+    });
+
+    res.redirect('../data');
+}
+
+module.exports.getSearchScoreRecordData = (req,res) =>{
+    let currentPage = parseInt(req.query.page) || 1;
+    let itemPerPage = parseInt(req.query.size) || 5;    
+    
+    let begin = (currentPage-1) * itemPerPage;
+    let end = currentPage* itemPerPage ;
+
+    find('score_Record','subjectid',req.query['search-name'])
+    .then(result =>{
+        res.render('../views/scoreRecord/scoreRecord-data-view',{                
+            listScoreRecord: result[0],
+            page : currentPage,
+            item : itemPerPage,
+            total: result[0].length                
+        });
+    });  
 }
--- controllers/studentDetail-controller.js
+++ controllers/studentDetail-controller.js
@@ -1,4 +1,6 @@
 const conn = require('../public/scripts/config');
+const find = require('../admin_modules/find');
+const date = require('date-and-time');
 
 module.exports.getStudentDetailData =  (req,res) => {                
     // PAGINATION    
@@ -17,15 +19,50 @@ module.exports.getStudentDetailData =  (req,res) => {
     
     let begin = (currentPage-1) * itemPerPage;
     let end = currentPage* itemPerPage ;
+    var sql,
+        sort='desc';
+    switch (req.query.column) {
+        case 'id':
+            makeSort('id',req.query.sort);
+            break;
+        case 'dob':
+            makeSort('dob',req.query.sort);
+            break;
+        case 'gender':
+            makeSort('gender',req.query.sort);
+            break;
+        case 'city':
+            makeSort('city',req.query.sort);
+            break;
+        case 'address':
+            makeSort('address',req.query.sort);
+            break;
+        default:
+            makeSort('id',req.query.sort);
+            break;
+    }
     
-    const sql =`SELECT * FROM STUDENT_DETAIL`;
+    function makeSort(column,state){
+        if(state === 'desc'){
+            sql =`SELECT * FROM STUDENT_DETAIL ORDER BY ${column} DESC`;
+            sort='asc'
+        }
+        else{
+            sql =`SELECT * FROM STUDENT_DETAIL ORDER BY ${column} ASC`;
+        }
+    }
     
     conn.query(sql, (err,result) => {
-        if(err) throw err;                    
+        if(err) throw err;    
+        for(x of result){
+            x['DoB'] = date.format(x['DoB'],'YYYY-MM-DD');
+        }                
         res.render('../views/studentDetail/studentDetail-data-view',{                
-            listFalculty: result.slice(begin, end),
+            listStudentDetail: result.slice(begin, end),
             page : currentPage,
-            item : itemPerPage                
+            item : itemPerPage,
+            total: result.length,
+            sort: sort                
         });
     });    
 
@@ -35,6 +72,63 @@ module.exports.getStudentDetailCreate = (req,res) => {
     res.render('../views/studentDetail/studentDetail-create-view');
 }
 
-module.exports.getStudentDetailModify = (req,res) => {
-    res.render('../views/studentDetail/studentDetail-modify-view');
+module.exports.postStudentDetailCreate = (req,res) =>{
+    console.log(req.body)
+    if (req.body.studentid) {
+        const sql =`INSERT INTO STUDENT_DETAIL VALUES ('${req.body.studentid}','${req.body.dob}','${req.body.gender}','${req.body.city}','${req.body.address}','${req.body.phone}')`;
+        conn.query(sql,(err,result)=>{
+            if(err) throw err;
+            console.log('call me',result);
+        });
+    }
+    res.redirect('/studentDetail/data');
+}
+
+module.exports.deleteStudentDetailData = (req,res) =>{
+    console.log('ggwp',req.params);
+    const sql = `DELETE FROM STUDENT_DETAIL WHERE ID = '${req.params.id}'`;
+    conn.query(sql,(err,result)=>{
+        if (err) throw err;
+        console.log(result);   
+    });
+
+    res.redirect('/studentDetail/data');
+     
+};
+
+module.exports.modifyStudentDetailData = (req,res) =>{
+    res.render('./studentDetail/studentDetail-modify-view',{
+        data: req.params
+    });
+};
+
+module.exports.postModifyStudentDetailData = (req,res) =>{
+    console.log(req.body)
+    const sql = `UPDATE STUDENT_DETAIL SET ID = '${req.body.studentid}', DoB = '${req.body.dob}', Gender = '${req.body.gender}' , City = '${req.body.city}, Address = '${req.body.address}, Phone = '${req.body.phone}'WHERE Id = '${req.body.studentid}'`;
+   
+    conn.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+    });
+
+    res.redirect('../data');
+}
+
+module.exports.getSearchStudentDetailData = (req,res) =>{
+    let currentPage = parseInt(req.query.page) || 1;
+    let itemPerPage = parseInt(req.query.size) || 5;    
+    
+    let begin = (currentPage-1) * itemPerPage;
+    let end = currentPage* itemPerPage ;
+
+    find('student_detail','id',req.query['search-name'])
+    .then(result =>{
+        result[0][0]['DoB'] = date.format(result[0][0]['DoB'],'YYYY-MM-DD');
+        res.render('../views/studentDetail/studentDetail-data-view',{                
+            listStudentDetail: result[0],
+            page : currentPage,
+            item : itemPerPage,
+            total: result[0].length                
+        });
+    });  
 }
