c3c8f8a8c45841eba1cc0d55e939ec192d5b23b1
nmtoan251998 <nmtoan251998@gmail.com>
2018-11-27 19:12:43 +0700

    Change the function file

--- controllers/functions-controller.js
+++ controllers/functions-controller.js
@@ -1,2 +1,9 @@
 const conn = require('../public/scripts/config');
 
+module.exports.getFunctions =  (req,res) => {   
+    res.render('../views/higher-order/functions-view');
+}
+
+module.exports.postFunctions =  (req,res) => {   
+    res.redirect('/functions')
+}
